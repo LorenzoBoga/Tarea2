d58201075751c6ad7a90c978078645bae2cf5262
Lorenzo Boga <lorenzoboga10@gmail.com>
2020-04-06 19:29:20 -0300

    Agregada funcion calculo de impuestos

---
+++ FuncionesUsuario.java
@@ -0,0 +1,18 @@
+public Class FuncionesUsuario{
+	public int CalculoDeImpuestos(Usuario user){
+		int aPagar = 0;
+		if( (user.getEdad() > 17 && user.getEdad() < 31) || user.getEdad() >65 ){
+			aPagar = 2000;
+		}else{
+			if(user.getEdad() <66){
+				aPagar = 7000;
+			}
+		}
+		int conDescuento = aPagar - aPagar*0.03*user.getCantidadHijos()	;
+		if(aPagar/2 > conDescuento){
+			return aPagar/2;	
+		}else{
+			return conDescuento;
+		}
+	}
+}
