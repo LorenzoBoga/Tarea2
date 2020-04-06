e0bf2f70f03837c3678d488a119e17085dd19c89
Lorenzo Boga <lorenzoboga10@gmail.com>
2020-04-06 19:20:46 -0300

    Agregada clase usuario solo atributos

---
+++ Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    private String nombre;
+    private int cedula;
+    private String contraseña;
+    private int edad;
+    private int cantidadHijos;
+    private int saldo;
+}
